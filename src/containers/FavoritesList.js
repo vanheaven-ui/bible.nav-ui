688fe823ac2bdd27fdc054080241644a995fae84
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-08 14:36:30 +0300

    Remove redundant code lines

--- src/containers/FavoritesList.js
+++ src/containers/FavoritesList.js
@@ -1,15 +1,17 @@
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import Favorite from '../components/Favorite';
+import { ridFavorite } from '../redux/actions';
 import styles from '../styles/favorites.module.css';
 
 const FavoritesList = () => {
-  const [favorites, setFavorites] = useState([]);
-  console.log(favorites.length);
+  // const [favorites, setFavorites] = useState([]);
+  // console.log(favorites.length);
   const hist = useHistory();
+  const dispatch = useDispatch();
 
-  const { user, jwt: token } = useSelector(state => state.user);
+  const { user, jwt: token, favorites } = useSelector(state => state.user);
 
   const removeFavorite = id => {
     console.log(id);
@@ -20,42 +22,23 @@ const FavoritesList = () => {
         credentials: 'include',
       },
     })
-      .then(() => console.log('Deleted!'))
+      .then(() => {
+        console.log('Deleted!');
+        dispatch(ridFavorite(id));
+      })
       .catch(err => console.log(err.message));
   };
 
   useEffect(() => {
     if (!user) {
       hist.push('/');
-    } else {
-      fetch(`https://biblenav-api.herokuapp.com/api/v1/users/${user.id}/favorites`, {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: `${token}`,
-          credentials: 'include',
-        },
-      })
-        .then(res => {
-          if (!res.ok) {
-            throw Error('Not successful');
-          }
-          console.log('Successful');
-          return res.json();
-        })
-        .then(data => {
-          console.log(data);
-          setFavorites(data.favorites);
-        })
-        .catch(err => console.log(err.message));
     }
   }, []);
   return (
     <section className={styles.favorites}>
       <h2>Your Favorites</h2>
       <div className={styles.favorite__list}>
-        <Favorite favorites={favorites} removeFavorite={removeFavorite} />
+        <Favorite favorites={favorites.favorites} removeFavorite={removeFavorite} />
       </div>
     </section>
   );
