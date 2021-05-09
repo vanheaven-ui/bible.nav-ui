79aca2140927edba7a91ac7486eeb9c486f1c1ed
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-09 10:49:31 +0300

    Create and style a 404 not found page

---
+++ src/components/404NotFound.js
@@ -0,0 +1,9 @@
+import '../styles/notfound.css';
+
+const NotFound = () => (
+  <section className="not-found">
+    <div className="overlay-notfound" />
+  </section>
+);
+
+export default NotFound;
