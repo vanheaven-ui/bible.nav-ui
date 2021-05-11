0513795ff5935ce7c3005e54395f22dfac6b9c89
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-11 14:15:36 +0300

    Create a BookLayout component

---
+++ src/components/BookLayout.js
@@ -0,0 +1,62 @@
+import { Container } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+const BookLayout = ({ params, loadVerse, getVerse }) => {
+  console.log(params.chapters);
+  const chapters = params.chapters.filter(chapter => chapter.number !== 'intro');
+  console.log(typeof chapters);
+  return (
+    <div className="book-details_wrap">
+      <header>
+        <h2>
+          The book of
+          {' '}
+          {params.bookName}
+        </h2>
+        <p>
+          Enjoy the
+          {' '}
+          {chapters.length}
+          {' '}
+          chapters of this book. Click on each chapter number to get the verses.
+        </p>
+      </header>
+      <Container className="chapter-list">
+        <h4>Chapters</h4>
+        {' '}
+        { params.chapters && chapters.map(chapter => (
+          <button
+            type="button"
+            id={chapter.id}
+            key={chapter.id}
+            onClick={e => loadVerse(e)}
+          >
+            {chapter.number}
+          </button>
+        ))}
+      </Container>
+      <Container className="verse-list">
+        <h5>Verses</h5>
+        {' '}
+        { params.verses && params.verses.map(verse => (
+          <button
+            type="button"
+            id={verse}
+            key={verse}
+            onClick={e => getVerse(e)}
+          >
+            {verse}
+          </button>
+        ))}
+      </Container>
+    </div>
+  );
+};
+
+BookLayout.propTypes = {
+  params: PropTypes.instanceOf(Object).isRequired,
+  loadVerse: PropTypes.func.isRequired,
+  getVerse: PropTypes.func.isRequired,
+};
+
+export default BookLayout;
