7e99c3b3f6b1cec1631023731c50835b1536c0b2
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-13 23:47:46 +0300

    Remove favorites action cases

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,11 +1,9 @@
 import {
-  ADD_FAVORITE,
-  CURRENT_USER, GET_FAVORITES, REMOVE_FAVORITE, REMOVE_USER,
+  CURRENT_USER, REMOVE_USER,
 } from '../actionTypes';
 
 const initState = {
   user: {},
-  favorites: [],
 };
 
 const userReducer = (state = initState, action) => {
@@ -18,24 +16,8 @@ const userReducer = (state = initState, action) => {
     case REMOVE_USER:
       return {
         ...state,
-        favorites: [],
         user: {},
         jwt: '',
-      };
-    case GET_FAVORITES:
-      return {
-        ...state,
-        favorites: action.payload,
-      };
-    case REMOVE_FAVORITE:
-      return {
-        ...state,
-        favorites: state.favorites.filter(favorite => favorite.id !== action.id),
-      };
-    case ADD_FAVORITE:
-      return {
-        ...state,
-        favorites: [...state.favorites, action.payload],
       };
     default:
       return state;
