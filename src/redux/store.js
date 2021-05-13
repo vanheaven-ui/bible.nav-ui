99b8fdd549fa981f03e9e3882d7a8b3485d186d7
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-13 23:44:44 +0300

    Configure redux store with enhancers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers';
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // eslint-disable-line
-);
+const middlewareEnhancer = applyMiddleware(thunkMiddleware);
+const composedEnhancer = composeWithDevTools(middlewareEnhancer);
+
+const store = createStore(rootReducer, undefined, composedEnhancer);
 
 export default store;
