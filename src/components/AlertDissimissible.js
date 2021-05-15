09f776c2d03b0d4dcb1405bb9cbaf39af9b45bc2
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-05-15 11:43:41 +0300

    Restruture alert layout

--- src/components/AlertDissimissible.js
+++ src/components/AlertDissimissible.js
@@ -1,18 +1,27 @@
 import PropTypes from 'prop-types';
 import '../styles/alert.css';
 
-const AlertDisimissible = ({ error, handleClick, click }) => (
-  <p className={click ? 'show' : 'hide'}>
-    <i className="fas fa-exclamation-triangle" />
-    {error}
-    <button
-      type="button"
-      onClick={e => handleClick(e)}
-    >
-      X
-    </button>
-  </p>
-);
+const AlertDisimissible = ({ error, handleClick, click }) => {
+  console.log(typeof (error));
+  return (
+    <div className={click ? 'my-alert show' : 'my-alert hide'}>
+      <p className="heading">
+        <i className="fas fa-exclamation-triangle" />
+        <button
+          type="button"
+          onClick={e => handleClick(e)}
+        >
+          X
+        </button>
+      </p>
+      <div className="messages">
+        {error.split(',').map(msg => (
+          <p key={msg}>{msg}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 AlertDisimissible.propTypes = {
   error: PropTypes.string.isRequired,
